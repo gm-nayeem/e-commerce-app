2244854ede4124f5246347faaae105fdcc6a7d6c
golam mostafa <gmnayeem25@gmail.com>
2022-12-17 21:50:42 +0600

    add single product  section

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 // internal import
 import Home from './pages/Home';
+import Product from './pages/Product';
 import ProductList from './pages/ProductList';
 
 function App() {
   return (
     <>
-      <ProductList />
+      <Product />
     </>
   );
 }
