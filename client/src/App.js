ecccb11052fc2d90ff3f1cc437c93d64fffdf3ff
golam mostafa <gmnayeem25@gmail.com>
2022-12-18 00:48:52 +0600

    create login form and cart section

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 // internal import
+import Cart from './pages/Cart';
 import Home from './pages/Home';
+import Login from './pages/Login';
 import Product from './pages/Product';
 import ProductList from './pages/ProductList';
 import Register from './pages/Register';
@@ -7,7 +9,7 @@ import Register from './pages/Register';
 function App() {
   return (
     <>
-      <Register />
+      <Cart />
     </>
   );
 }
