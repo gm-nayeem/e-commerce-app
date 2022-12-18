6402375aa4e78f10a3e222e27db50b219372c3dd
golam mostafa <gmnayeem25@gmail.com>
2022-12-18 19:04:22 +0600

    basic setup for server

--- api/index.js
+++ api/index.js
@@ -1,13 +1,45 @@
+// external import
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+// internal import 
+
+
 const app = express()
+
+// middlewares
+app.use(morgan('dev'))
+app.use(cors())
+
+// body-parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// routes
+
+
 
 app.get('/', (req, res) => {
     res.json({
-        message: "Welcome to our application"
+        message: "Welcome to our E-commerce application"
     })
 });
 
-const PORT = process.env.PORT || 4000;
+
+// port and database connection
+const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log(`Server in running at http://localhost:${PORT}`);
+    mongoose.connect(
+        process.env.MONGO_URL,
+        { useNewUrlParser: true },
+        () => {
+            console.log('Database Connected Successfully...');
+        }
+    );
 })
+
